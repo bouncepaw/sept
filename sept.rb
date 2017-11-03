4ca4381d73f8d37f825c1d90b8dfa6db061fdb4d
Timur Ismagilov <tims102rus@gmail.com>
2017-11-03 21:26:21 +0500

    version 1.1.2: full params support adn code improvement

--- sept.rb
+++ sept.rb
@@ -1,6 +1,4 @@
-require 'sxp'
-require 'colorize'
-require 'pp'
+%w(sxp colorize).each { |f| require f }
 
 # Markup as it should have been
 # Write your page as s-expression structure
@@ -21,37 +19,37 @@ require 'pp'
 #         (td SEPT)
 #         (td %{param})))))
 class Sept
-
-  def initialize(params)
+  def initialize(params, files_to_parse)
     @params = params
     @html = ''
+
+    files_to_parse.each { |f| self.cook f }
   end
 
 
   # Function that 'cooks' passed file. It logs some info. The name is bad
-  def self.cook(filename)
+  def cook(filename)
     unless File.extname(filename) == ".sept"
-      print "HEY! ".red, "SEPT files must end with `.sept` extension, ",
-        "so can't parse `#{filename}`\n"
+      puts "#{filename} is not `.sept` file, so can't parse it!".red
       return
     end
     @html = ''
 
     file = File.read filename
-    puts "got #{filename}, its content is: #{file}"
+    puts "got #{filename}, its content is:\n#{file}"
 
     new_filename = filename[0..-6] + ".html"
-    new_file = self.to_html(self.to_s(SXP.read(file))) % @params,
+    new_file = self.to_html(self.to_s(SXP.read(file))) % @params
 
     # Create file if it does not exist yet
-    File.new new_filename, "w+" unless File.file? new_filename
+    File.new new_filename, 'w+' unless File.file? new_filename
     File.write new_filename, new_file
-    puts "parsed #{filename} and saved in #{new_filename}: #{new_file}"
+    puts "parsed #{filename} and saved in #{new_filename}:\n #{new_file}"
   end
 
   # Recursive function that turns everything in `node` to a string
   # [:sym, [1, "str"]] => ["sym", ["1", "str"]]
-  def self.to_s(node)
+  def to_s(node)
     node.each_with_index do |sub, i|
       # Is it hacker-ish?
       sub.is_a?(Array) ? self.to_s(sub) : node[i] = sub.to_s
@@ -59,7 +57,7 @@ class Sept
   end
 
   # Function that turns array to something usable
-  def self.to_html(node)
+  def to_html(node)
     if node.is_a? Array
       if node.length == 1 then @html << "<#{node[0]}/>"
       else
@@ -67,17 +65,14 @@ class Sept
         node[1..-1].each { |e| self.to_html e }
         @html << "</#{node[0].split(' ')[0]}>"
       end
-    else
-      @html << node
+    else @html << node
     end
 
     @html
   end
 end
 
-if ARGV.length == 0
-  puts 'SEPT:'.red, "Too few arguments"
-else case ARGV[0]
+ARGV.length == 0 ? puts("Too few arguments".red) : case ARGV[0]
 when "-h" # help
   puts 'S-Expression Powered Template HyperText Markup Language',
     'To get version, run `sept -v`',
@@ -90,13 +85,12 @@ when "-h" # help
 when "-v" # version
   puts "SEPT HTML version 1.1.1"
 when "-d" # data
-  sept = Sept.new(eval ARGV[1])
-  ARGV[2..-1].each { |f| sept.cook f }
+  puts "Passed data as argument"
+  sept = Sept.new(eval(ARGV[1]), ARGV[2..-1])
 when '-f' # file
-  sept = Sept.new(eval File.read ARGV[1])
-  ARGV[2..-1].each { |f| sept.cook f }
+  puts "Passed data as file"
+  sept = Sept.new(eval(File.read ARGV[1]), ARGV[2..-1])
 else
-  sept = Sept.new({})
-  ARGV.each { |f| sept.cook f }
-end
+  puts "No passed data"
+  sept = Sept.new({}, ARGV)
 end
